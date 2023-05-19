111ec97346a2ea7cfcca1764ca0afcc72bc40764
Valeriy Kozlov <Valera.31.07.9.9@gmail.com>
2023-05-19 10:30:31 +0300

    front with new dependensies

--- server/src/products/providers/products.providers.ts
+++
@@ -1,9 +0,0 @@
-import { PRODUCTS_REPOSITORY } from '../constants/products.constants';
-import { Product } from '../../schemas/product.schema';
-
-export const ProductsProviders = [
-  {
-    provide: PRODUCTS_REPOSITORY,
-    useValue: Product,
-  },
-];
--- server/src/users/providers/users.providers.ts
+++
@@ -1,9 +0,0 @@
-import { User } from 'src/schemas/user.schema';
-import { USERS_REPOSITORY } from '../constants/users.constants';
-
-export const UsersProviders = [
-  {
-    provide: USERS_REPOSITORY,
-    useValue: User,
-  },
-];
